394652dd4fee95973b49f1ef2d4545eac3c121e0
Aaron Frey <aaron.frey@gmail.com>
2017-12-06 15:30:38 -0500

    style location desktop

--- location.php
+++ location.php
@@ -11,15 +11,52 @@
       
     <?php include 'partials/header.php'; ?>
 
-    <div class="container content">
-      <div class="row">
-        <div class="col-md-12">
-          This is where location content goes
-        </div>
+    <div class="main">
+
+      <div class="banner">
+        <img src="img/location/banner.png" class="img-responsive">
       </div>
-    </div>
+
+      <div class="container">
+
+        <div class="row">
+
+          <div class="content clearfix">
+
+            <div class="col-md-9">
+
+              <h1>Location</h1>
+
+              <h2>Superior Access to the Waterfront</h2>
+
+              <p>Strategically located, The Camden Waterfront has central access to major highways, roadways, and mass transit systems.</p>
+
+              <ul>
+                <li>Easily accessible to interstates I-95, I-676, I-76, and I-295</li>
+                <li>Convenient access to other major roadways, including SR-130, SR-38, and SR-70, as well as Philadelphia International Airport and 30th Street Station (Amtrak)</li>
+                <li>Public transportation via PATCO Speedline, NJ Transit River LINE, and RiverLink Ferry, as well as Walter Rand Transportation Center and City Hall Station</li>
+                <li>Dedicated pedestrian and bicycle paths and the Benjamin Franklin Bridge, supporting thousands who drive, walk, and bike between the City of Camden, Greater Camden County, and Center City Philadelphia</li>
+                <li>Dedicated Camden Waterfront shuttle to major mass transit drop-off locations</li>
+              </ul>
+
+              <br><br>
+
+            </div>
+
+            <div class="col-md-12">
+              <img src="img/location/map.png" class="img-responsive">
+            </div>
+
+          </div><!-- .content -->
+
+        </div><!-- .row -->
+
+      </div><!-- .container -->
+
+    </div><!-- .main -->
 
     <?php include 'partials/footer.php'; ?>
 
   </body>
+
 </html>
