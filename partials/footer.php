f7422acef651f6c0f0cc8291880661a5eb14d9dd
Aaron Frey <aaron.frey@gmail.com>
2017-12-06 13:50:17 -0500

    finalized footer styles

--- partials/footer.php
+++ partials/footer.php
@@ -1,28 +1,45 @@
 <footer class="footer">
 
-	<div class="footer-container clearfix">
+	<div class="footer-container container-fluid">
 
-		<div class="col-md-3 col-sm-12 liberty">
-			<img src="img/liberty.svg" class="img-responsive">
-			<span class="copywrite">&copy; Copyright <?php echo date('Y'); ?>. All rights reserved.</span>
+		<div class="row visible-xs visible-sm">
+			<div class="col-md-12">
+				For leasing information, please contact:
+			</div>
 		</div>
 
-		<div class="col-md-3 col-sm-4">
-			<div class="title">Anne M. Cummins</div>
-			<a target="_blank" href="tel:2152557607">215.255.7607</a>
-			<a target="_blank" href="mailto:acummins@libertyproperty.com">acummins@libertyproperty.com</a>
+		<div class="row">
+
+			<div class="col-md-3 hidden-xs hidden-sm liberty top">
+				<img src="img/liberty.svg" class="img-responsive">
+				<span class="copywrite">&copy; Copyright <?php echo date('Y'); ?>. All rights reserved.</span>
+			</div>
+
+			<div class="col-md-3 col-sm-4">
+				<div class="title">Anne M. Cummins</div>
+				<a target="_blank" href="tel:2152557607">215.255.7607</a>
+				<a target="_blank" href="mailto:acummins@libertyproperty.com">acummins@libertyproperty.com</a>
+			</div>
+
+			<div class="col-md-3 col-sm-4">
+				<div class="title">Janice Wong</div>
+				<a target="_blank" href="tel:2152557645">215.255.7645</a>
+				<a target="_blank" href="mailto:jwong@libertyproperty.com">jwong@libertyproperty.com</a>
+			</div>
+
+			<div class="col-md-3 col-sm-4">
+				<div class="title">Tony Ewing</div>
+				<a target="_blank" href="tel:2152557627">215.255.7627</a>
+				<a target="_blank" href="mailto:tewing@libertyproperty.com">tewing@libertyproperty.com</a>
+			</div>
+
 		</div>
 
-		<div class="col-md-3 col-sm-4">
-			<div class="title">Janice Wong</div>
-			<a target="_blank" href="tel:2152557645">215.255.7645</a>
-			<a target="_blank" href="mailto:jwong@libertyproperty.com">jwong@libertyproperty.com</a>
-		</div>
-
-		<div class="col-md-3 col-sm-4">
-			<div class="title">Tony Ewing</div>
-			<a target="_blank" href="tel:2152557627">215.255.7627</a>
-			<a target="_blank" href="mailto:tewing@libertyproperty.com">tewing@libertyproperty.com</a>
+		<div class="row">
+			<div class="visible-xs visible-sm liberty bottom">
+				<img src="img/liberty.svg" class="img-responsive">
+				<span class="copywrite">&copy; Copyright <?php echo date('Y'); ?>. All rights reserved.</span>
+			</div>			
 		</div>
 
 	</div>
