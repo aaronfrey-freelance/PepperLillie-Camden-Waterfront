2e7b9312cb27a465384b92c69e2f4439550694a8
Aaron Frey <aaron.frey@gmail.com>
2017-12-06 01:19:15 -0500

    style mobile menu

--- partials/header.php
+++ partials/header.php
@@ -3,7 +3,7 @@
   <nav class="navbar">
 
     <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header vertical-align">
+    <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-primary" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
