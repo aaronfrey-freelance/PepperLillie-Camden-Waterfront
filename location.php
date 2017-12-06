f6bddf55dfd0b5c7a59c3df1619fe834283a1639
Aaron Frey <aaron.frey@gmail.com>
2017-12-06 02:01:29 -0500

    add location page

---
+++ location.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+  <?php
+    $page_title = "The Camden Waterfront | Location";
+    $page_description = "The Camden Waterfront Website";
+    include 'partials/head.php';
+  ?>
+
+  <body id="<?php echo basename($_SERVER['PHP_SELF'], ".php"); ?>">
+      
+    <?php include 'partials/header.php'; ?>
+
+    <div class="container content">
+      <div class="row">
+        <div class="col-md-12">
+          This is where location content goes
+        </div>
+      </div>
+    </div>
+
+    <?php include 'partials/footer.php'; ?>
+
+  </body>
+</html>
