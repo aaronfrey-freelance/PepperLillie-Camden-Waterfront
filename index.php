433868e4c8af365ee0dfab84bef7b22d376382d8
Aaron Frey <aaron.frey@gmail.com>
2017-12-08 14:16:14 -0500

    style homepage

--- index.php
+++ index.php
@@ -11,10 +11,20 @@
       
     <?php include 'partials/header.php'; ?>
 
-    <div class="container content">
-      <div class="row">
-        <div class="col-md-12">
-          This is where content goes
+    <div class="home-banner">
+      <a href="#" class="btn btn-primary video_tour">Video Tour <img src="img/play_button.png"></a>
+      <img src="img/home/banner.jpg" class="img-responsive">
+      <span>The Camden Waterfront</span>
+    </div>
+
+    <div class="content text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-offset-1 col-md-10">
+            <h1>The Future on the Horizon</h1>
+            <h2>The Camden Waterfront is an entirely new mixed-use urban neighborhood on the banks of the Delaware River with dramatic views of the stunning Philadelphia skyline and Benjamin Franklin Bridge.</h2>
+            <h2>Defined by progressive architecture and vibrant activity throughout the day, The Camden Waterfront will consist of up to 1.2 million square feet of high-performance office space, a 180-room hotel, apartment living, retail amenities and an expanded and improved public park.</h2>
+          </div>
         </div>
       </div>
     </div>
