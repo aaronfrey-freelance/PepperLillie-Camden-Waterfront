829c9d583e0e13e675bfbffca3a640e22a300f32
Aaron Frey <aaron.frey@gmail.com>
2017-12-08 20:20:48 -0500

    add logic to not show submenus

--- js/main.js
+++ js/main.js
@@ -1,7 +1,31 @@
+var windowWidth = 0;
+
+// Gets the actual window width, not counting scrollbar
+function getWindowWidth() {
+  if (typeof(window.innerWidth) == 'number') {
+    windowWidth = window.innerWidth;
+  } else {
+    if (document.documentElement && document.documentElement.clientWidth) {
+      windowWidth = document.documentElement.clientWidth;
+    } else {
+      if (document.body && document.body.clientWidth) {
+        windowWidth = document.body.clientWidth;
+      }
+    }
+  }
+  return windowWidth;
+}
+
 (function() {
 
+  $(window).on("load resize", function(e) {
+    getWindowWidth();
+  });
+
   $('ul.nav li.dropdown').hover(function() {
-    $(this).find('.dropdown-menu').stop(true, true).fadeIn(500);
+    if (windowWidth >= 768) {
+      $(this).find('.dropdown-menu').stop(true, true).fadeIn(500);
+    }
   }, function() {
     $(this).find('.dropdown-menu').stop(true, true).fadeOut(500);
   });
