1ebd8ffca645c22ef0c2395ca947fce46739f886
Aaron Frey <aaron.frey@gmail.com>
2017-12-11 13:51:38 -0500

    tweak mobile menu

--- js/main.js
+++ js/main.js
@@ -29,7 +29,9 @@ function getWindowWidth() {
       $(this).find('.dropdown-menu').stop(true, true).fadeIn(500);
     }
   }, function() {
-    $(this).find('.dropdown-menu').stop(true, true).fadeOut(500);
+    if (windowWidth >= 768) {
+      $(this).find('.dropdown-menu').stop(true, true).fadeOut(500);
+    }
   });
 
   // Homepage Image Pan
--- views.php
+++ views.php
@@ -15,14 +15,22 @@
       <img src="img/views/banner.jpg" class="img-responsive">
     </div>
 
-    <div class="container content">
+    <div class="container">
+
       <div class="row">
-        <div class="col-md-offset-1 col-md-10">
-          <h1>Views From the Waterfront</h1>
-          <p>The Camden Waterfront is a highly visible location on the Delaware River. Each of the buildings within The Camden Waterfront Masterplan has sweeping panoramic views of the Waterfront Park, the Delaware River, the stunning Philadelphia skyline and the classically elegant Benjamin Franklin Bridge.</p>
-          <img src="img/views/night-time.png" class="img-responsive">
+
+        <div class="content clearfix">
+
+          <div class="col-md-offset-1 col-md-10">
+            <h1>Views From the Waterfront</h1>
+            <p>The Camden Waterfront is a highly visible location on the Delaware River. Each of the buildings within The Camden Waterfront Masterplan has sweeping panoramic views of the Waterfront Park, the Delaware River, the stunning Philadelphia skyline and the classically elegant Benjamin Franklin Bridge.</p>
+            <img src="img/views/night-time.png" class="img-responsive">
+          </div>
+
         </div>
+
       </div>
+      
     </div>
 
     <?php include 'partials/footer.php'; ?>
