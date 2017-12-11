ef763481c57c292e61ada6fc2d24370654035c0d
Aaron Frey <aaron.frey@gmail.com>
2017-12-11 18:23:47 -0500

    remove php from urls

--- partials/header.php
+++ partials/header.php
@@ -10,7 +10,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/index.php">
+      <a class="navbar-brand" href="index">
         <img src="img/logo.png" class="img-responsive">
       </a>
     </div>
@@ -18,23 +18,25 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="navbar-collapse-primary">
       <div class="mobile-brand">
-        <img src="img/logo.png" class="img-responsive">
+        <a href="index">
+          <img src="img/logo.png" class="img-responsive">
+        </a>
       </div>
       <ul class="nav navbar-nav">
         <li class="dropdown current">
           <a class="dropdown-toggle" data-toggle="dropdown" role="button">Why Camden Waterfront</a>
           <ul class="dropdown-menu">
             <li><a class="btn disabled">Why Camden Waterfront</a></li>
-            <li><a href="location.php">Location</a></li>
-            <li><a href="tax-incentives.php">Tax Incentives</a></li>
-            <li><a href="talent-attraction.php">Talent Attraction</a></li>
-            <li><a href="views.php">Views</a></li>
+            <li><a href="location">Location</a></li>
+            <li><a href="tax-incentives">Tax Incentives</a></li>
+            <li><a href="talent-attraction">Talent Attraction</a></li>
+            <li><a href="views">Views</a></li>
             <li><a href="https://www.youtube.com/watch?v=DtBocfjf1l8&app=desktop" target="_blank">Video Tour</a></li>
           </ul>
         </li>
-        <li><a href="the-waterfront-community.php">The Waterfront Community</a></li>
-        <li><a href="build-to-suit-opportunities.php">Build-to-Suit-Opportunities</a></li>
-        <li><a href="about-liberty-property-trust.php">About Liberty Property Trust</a></li>
+        <li><a href="the-waterfront-community">The Waterfront Community</a></li>
+        <li><a href="build-to-suit-opportunities">Build-to-Suit-Opportunities</a></li>
+        <li><a href="about-liberty-property-trust">About Liberty Property Trust</a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
 
