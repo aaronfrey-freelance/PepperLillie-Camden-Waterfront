e959f8effe14178a1a99289e49a606e9a44a2b05
Aaron Frey <aaron.frey@gmail.com>
2017-12-12 10:48:00 -0500

    talent page updates

--- talent-attraction.php
+++ talent-attraction.php
@@ -28,7 +28,7 @@
             <div class="col-xs-4">
               <div class="rating">
                 <div class="rank">36<sup>%</sup></div>
-                Residents with<br>Bachelors Desgrees +
+                Residents with<br>Bachelor's Degrees +
               </div>
             </div>
             <div class="col-xs-4">
@@ -47,7 +47,7 @@
         <div class="row">
           <div class="col-md-12">
             <p>The Camden Waterfront is in close proximity to the talented and well-educated workforce that exists in Philadelphia and South Jersey.  Approximately 36 percent of Philadelphia area residents hold bachelor’s degrees or higher, compared with the national average of 29 percent.  The college graduate retention rate in the region has grown rapidly in recent years to an all-time high of 65 percent. With a regional workforce of 3.1 million, employers on The Camden Waterfront will have access to a stable workforce that is diverse in training, skills and experience.</p>
-            <p>In addition to education attainment, Philadelphia area’s millennial growth over the past decade is the highest of any of the nation’s 30 largest cities. This growth is a result of the area's world-class universities, favorable cost of living, outstanding transportation infrastructure, and culture.  The growth of the millennial population in Philadelphia has been concentrated in neighborhoods very accessible to The Camden Waterfront, such as Northern Liberties, Old City and South Philly.  The metro region has become a rapidly growing, energetic, creative community that is attracting and retaining young and innovative talent every day.</p>
+            <p>In addition to education attainment, Philadelphia area’s Millennial growth over the past decade is the highest of any of the nation’s 30 largest cities. This growth is a result of the area's world-class universities, favorable cost of living, outstanding transportation infrastructure, and culture.  The growth of the Millennial population in Philadelphia has been concentrated in neighborhoods very accessible to The Camden Waterfront, such as Northern Liberties, Old City, and South Philly.  The metro region has become a rapidly growing, energetic, creative community that is attracting and retaining young and innovative talent every day.</p>
             <span class="source">Source: Select Greater Philadelphia</span>
           </div>
         </div>
