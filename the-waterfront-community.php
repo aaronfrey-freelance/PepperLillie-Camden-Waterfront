275004872a71beca1afc086a271914c968d519d6
Aaron Frey <aaron.frey@gmail.com>
2017-12-11 22:46:56 -0500

    initial pass community page

--- the-waterfront-community.php
+++ the-waterfront-community.php
@@ -11,15 +11,91 @@
       
     <?php include 'partials/header.php'; ?>
 
-
-
-    <div class="container content">
-      <div class="row">
-        <div class="col-md-12">
-          This is where content goes
-        </div>
-      </div>
+    <div class="banner">
+      <img src="img/community/banner.png" class="img-responsive">
     </div>
+
+    <div class="container">
+
+      <div class="content">
+
+        <div class="row">
+          <div class="col-md-12">
+            <h1>The Camden Waterfront Community</h1>
+            <h2>Master Site Plan</h2>
+          </div>
+        </div>
+
+        <div class="row">
+
+          <div class="col-sm-8">
+            
+            <table class="table table-bordered">
+              <tbody>
+                <tr>
+                  <td class="title">77 Water Street</td>
+                  <td>50,000 RSF office build-to-suit option</td>
+                </tr>
+                <tr>
+                  <td class="title">55 Water Street</td>
+                  <td>Up to 400,000 RSF office build-to-suit option</td>
+                </tr>
+                <tr>
+                  <td class="title">Two Penn Street</td>
+                  <td>50,000-75,000 RSF office build-to-suit option</td>
+                </tr>
+                <tr>
+                  <td class="title">One Victor Place</td>
+                  <td>90,000 RSF office build-to-suit option</td>
+                </tr>
+                <tr>
+                  <td class="title">One Cooper Street</td>
+                  <td>Up to 75,000 RSF office build-to-suit option.</td>
+                </tr>
+                <tr>
+                  <td class="title">One Water Street</td>
+                  <td>American Water headquarters. Delivery: Fall 2018</td>
+                </tr>
+                <tr>
+                  <td class="title">One Riverside Drive</td>
+                  <td>380,000 SF office tower. Delivery: Spring 2020</td>
+                </tr>
+                <tr>
+                  <td class="title yellow">One Penn Street</td>
+                  <td>180-room Hilton Garden Inn Hotel. Delivery: Spring 2020</td>
+                </tr>
+                <tr>
+                  <td class="title grey">11 Cooper Street</td>
+                  <td>156 residential apartments. Delivery: Spring 2019</td>
+                </tr>
+                <tr>
+                  <td class="title grey">Residential</td>
+                  <td>Waterfront townhomes</td>
+                </tr>
+              </tbody>
+            </table>
+
+          </div>
+
+          <div class="col-sm-4">
+            
+            <div class="grey-box">
+              <ul>
+                <li>20 acres of waterfront property</li>
+                <li>Up to 1.2 million SF of high performance office space</li>
+                <li>Multiple office build-to-suit options customizable for distinct needs</li>
+                <li>4.4 acres of public park space</li>
+                <li>Retail amenities</li>
+              </ul>
+            </div>
+
+          </div>
+
+        </div><!-- row -->
+
+      </div><!-- content -->
+
+    </div><!-- container -->
 
     <?php include 'partials/footer.php'; ?>
 
