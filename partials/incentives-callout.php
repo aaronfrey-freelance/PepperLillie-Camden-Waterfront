64399291258416e140aec04a57eefbdef491821d
Aaron Frey <aaron.frey@gmail.com>
2017-12-12 16:39:59 -0500

    style tax incentives for mobile

---
+++ partials/incentives-callout.php
@@ -0,0 +1,11 @@
+<div class="call-out">
+  <h1>Grow NJ Awards</h1>
+  <p>Holtec International <span>($260M)</span></p>
+  <p>EMR Facility <span>($253M)</span></p>
+  <p>American Water HQ <span>($164M)</span></p>
+  <p>ResinTech <span>($138M)</span></p>
+  <p>Subaru <span>($117M)</span></p>
+  <p>Lockheed Martin <span>($107M)</span></p>
+  <p>76ers Practice Facility <span>($82M)</span></p>
+  <p>ACTEGA North America <span>($40M)</span></p>
+</div>
