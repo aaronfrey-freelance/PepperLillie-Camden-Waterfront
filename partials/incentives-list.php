2edc841976b5f66b92fdf0522639d4ac23dfebd1
Aaron Frey <aaron.frey@gmail.com>
2017-12-13 00:29:48 -0500

    style tax incentives desktop

---
+++ partials/incentives-list.php
@@ -0,0 +1,10 @@
+<div class="row">
+  <div class="col-sm-12 col-md-offset-2 col-md-8">
+    <ul>
+      <li>Over $1.65 billion dollars have been awarded by the New Jersey Economic Development Authority (NJEDA) through the Grow NJ program to a range of companies including American Water, Holtec International, Lockheed Martin, and Subaru.</li>
+      <li>All industries are eligible for the Grow NJ tax credit incentive, and bonuses are available for target industries.</li>
+      <li>Unused tax credits are transferable and saleable at a discount to face value.</li>
+      <li>The Grow New Jersey tax credit can be used towards the following costs associated with developing a new office building: site acquisition, soft costs (i.e., design and engineering), infrastructure construction, building construction, interior improvements, and relocation costs.</li>
+    </ul>
+  </div>
+</div> 
--- tax-incentives.php
+++ tax-incentives.php
@@ -11,19 +11,20 @@
       
     <?php include 'partials/header.php'; ?>
 
-    <div class="container">
+    <div class="container-fluid">
 
       <div class="content">
 
         <div class="row grey-bg">
-          <div class="col-md-12">
+          <div class="col-md-offset-2 col-md-8">
             <h1>Business Tax Incentive Program</h1>
           </div>
         </div><!-- .row -->
 
         <div class="row grey-bg">
 
-          <div class="col-md-4">
+          <div class="col-sm-5 col-md-offset-2 col-md-4">
+
             <h2>Grow New Jersey</h2>
             <p>Grow NJ is a powerful job creation and retention program helping qualified companies succeed in New Jersey.  Within the Grow NJ Program, Camden is designated as a Garden State Growth Zone (GSGZ), which means The Camden Waterfront projects can qualify for the highest levels of economic tax incentives.</p>
 
@@ -33,41 +34,35 @@
 
           </div>
 
-          <div class="col-md-8">
-            <img src="img/incentives/info-graphic-top.svg" class="img-responsive">
+          <div class="col-sm-7 col-md-5">
+            <img src="img/incentives/info-graphic-top.svg" class="infographic-top img-responsive">
+          </div>
+
+          <div class="hidden-xs">
+            <?php include 'partials/incentives-list.php'; ?>
           </div>
 
         </div><!-- .row -->
 
         <div class="visible-xs">
           <?php include 'partials/incentives-callout.php'; ?>
+          <?php include 'partials/incentives-list.php'; ?>
         </div>
-
-        <div class="row">
-          <div class="col-md-12">
-            <ul>
-              <li>Over $1.65 billion dollars have been awarded by the New Jersey Economic Development Authority (NJEDA) through the Grow NJ program to a range of companies including American Water, Holtec International, Lockheed Martin, and Subaru.</li>
-              <li>All industries are eligible for the Grow NJ tax credit incentive, and bonuses are available for target industries.</li>
-              <li>Unused tax credits are transferable and saleable at a discount to face value.</li>
-              <li>The Grow New Jersey tax credit can be used towards the following costs associated with developing a new office building: site acquisition, soft costs (i.e., design and engineering), infrastructure construction, building construction, interior improvements, and relocation costs.</li>
-            </ul>
-          </div>
-        </div> 
 
       </div><!-- .content -->
 
     </div><!-- .container -->
 
-    <div class="container">
+    <div class="container-fluid">
       
       <div class="row">
         
-        <div class="col-md-12">
+        <div class="col-md-offset-2 col-md-8">
           
           <img src="img/incentives/infographic-mobile.png" class="infographic visible-xs img-responsive">
           <img src="img/incentives/info-graphic-bottom.svg" class="infographic hidden-xs img-responsive">
 
-          <p class="text-center hidden-xs">
+          <p class="more-info text-center hidden-xs">
             For more information on the NJ Grow Program in Camden, please visit the New Jersey Economic<br>
             Development Authority (NJEDA) website: <a href="http://www.njeda.com/pdfs/GSGZ_Camden.aspx" target="_blank">http://www.njeda.com/pdfs/GSGZ_Camden.aspx</a><br>
             <span>*Businesses should consult with the NJEDA to determine their maximum tax credit eligibility.</span>
